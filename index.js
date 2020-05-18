723e43ba6ca46a437cb262d91c44fa1e0494aa6d
sara altaee <sara.korea96@gmail.com>
2020-05-18 21:34:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  // manipulate();
+  document.getElementById('text').textContent = "This is really cool!";
+});
 console.log(
   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
 );
